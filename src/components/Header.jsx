71c0c6355907e29ddf866b5109b5714849cfeb71
madruga665 <madruga665@hotmail.com>
2021-05-27 22:25:13 -0300

    implementação inicial do header

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./styles/Header.css";
+
+export default class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <h1>Mc Lojinha Feliz</h1>
+      </header>
+    );
+  }
+}
