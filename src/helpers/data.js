4ef72ba2c43bcb8bdd81acbd0c2983794403bac6
madruga665 <madruga665@hotmail.com>
2021-05-27 23:16:06 -0300

    add data com produdos

---
+++ src/helpers/data.js
@@ -0,0 +1,24 @@
+const products = [
+  {
+    name: 'Big Mac',
+    price: 22.90,
+    image: './images/bigmac.png'
+  },
+  {
+    name: 'Duplo Big Mac',
+    price: 24.90,
+    image: './images/duplo-bigmac.png'
+  },
+  {
+    name: 'Big Tasty',
+    price: 29.90,
+    image: './images/big-tasty.png'
+  },
+  {
+    name: 'Picanha Cheese Salada',
+    price: 31.90,
+    image: './images/picanha-cheese-salada.png'
+  }
+]
+
+export default products;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react'
-import Footer from '../components/Footer';
-import Header from '../components/Header'
-import ProductList from '../components/ProductList'
-import './styles/Home.css';
+import React, { Component } from "react";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import ProductList from "../components/ProductList";
+import "./styles/Home.css";
 
 export default class Home extends Component {
   render() {
@@ -12,6 +12,6 @@ export default class Home extends Component {
         <ProductList />
         <Footer />
       </main>
-    )
+    );
   }
 }
