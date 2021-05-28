e4957e86234bedf2c2711ca1302d6893cc430c82
madruga665 <madruga665@hotmail.com>
2021-05-28 14:44:39 -0300

    Add rotas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from 'react'
-import Home from './pages/Home'
+import React, { Component } from "react";
+import { Route, Switch } from "react-router";
+import Home from "./pages/Home";
+import ShoppingCart from "./pages/ShoppingCart";
 export default class App extends Component {
   render() {
     return (
-      <>
-        <Home />
-      </>
-    )
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/shopping-cart" component={ShoppingCart} />
+      </Switch>
+    );
   }
 }
-
---
+++ src/pages/ShoppingCart.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ShoppingCart extends Component {
+  render() {
+    return (
+      <section className="shopping-cart">
+        <h1>Carrinho de compras</h1>
+      </section>
+    )
+  }
+}
